537c628fead98a5a1ce0008f6c18926a5f20ab41
Eric Toshio <eric.toshio.e.s@gmail.com>
2019-03-23 18:50:52 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'product-info/:index', loadChildren: './product-info/product-info.module#ProductInfoPageModule' }
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'product-info/:index', loadChildren: './product-info/product-info.module#ProductInfoPageModule' },
+  { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [
